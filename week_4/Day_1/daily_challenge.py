eae3988a3e9098ab914975f46677c747674d78ed
inbarfi <inbarfi.dev@gmail.com>
2021-07-18 16:59:16 +0300

    DC Exercise 4

--- week_4/Day_1/daily_challenge.py
+++ week_4/Day_1/daily_challenge.py
@@ -18,17 +18,41 @@ elif user_input_length > 10:
 #2
 print(f"first character is: {user_input[0]}, last is: {user_input[-1]}")
 
-#3 - why does it print it weirdly?
-#print(f"{user_input[0]} \n {user_input[1]} \n {user_input[2]} \n {user_input[3]} \n {user_input[4]}")
+#3 - 
+print(f"{user_input[0]} \n{user_input[1]} \n {user_input[2]} \n {user_input[3]} \n {user_input[4]}")
 for i in user_input:
     user_input = i
     print(user_input)
-# =============================================================================
-# #4
-# import random
-# inbar = "inbar"
-# inbar_list = list(inbar)
-# print(inbar_list)
-# #print(random.shuffle(inbarr))
-# =============================================================================
+#4
+import random
+inbar = "inbar"
+inbar_list = list(inbar)
+print(inbar_list)
+
+random.shuffle(inbar_list)
+print(inbar_list) #shuffled
+
+#change the list to a str:
+shaufle_inbar = "".join(inbar_list)
+print(type(shaufle_inbar))
+print(shaufle_inbar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
