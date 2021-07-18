004cef00cf44cea6be7671dfb0088b397fe36df0
inbarfi <inbarfi.dev@gmail.com>
2021-07-18 13:15:00 +0300

    xp

--- week_3/Day_3/w3_d3_DC.js
+++ week_3/Day_3/w3_d3_DC.js
@@ -1,9 +1,25 @@
 // w3_d3_Daily Challenge: Show Only The Letters
-// Rina
-// Mickel - 
+//Create an input type text that takes/shows only letters.
+//Hint: use the keyup or the keydown events and remove any character that is not a letter.
+//Hint : Check out keycodes in Javascript or Regular Expressions
 
+input.addEventListener("keydown", typeLetters);
 
-let input = document.getElementById('text');
-let validate = document.createAttribute("onkeypress")
-validate.value = "return /[a-z]/i.test(event.key)"
-input.setAttributeNode(validate)
+// function typeLetters(event) {
+//     // console.log(event)
+// 	if (event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode == 8) { //backspca(delete)
+// 		input = document.getElementById("input").value; //no need for let because?
+// 	}
+// 	else {
+// 		event.preventDefault(); //preventing the typing default of keydown.
+// 	}
+// }
+
+// OR: doesnt work:
+
+function typeLetters(event) {
+    // console.log(event)
+	if (!event.keyCode >= 65 && event.keyCode <= 90 || event.keyCode == 8) { //backspca(delete)
+		event.preventDefault(); //preventing the typing default of keydown.
+	}
+}
---
+++ week_4/Day_1/xp.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 17 22:27:28 2021
+
+@author: Inbar
+"""
+
+name = "Inbar"
